979c2a407ed583fcb9db647dce6801276f7d96b0
Udayanga Wickramasinghe <udayanga@Udayangas-MacBook-Pro.local>
2015-11-18 02:41:48 -0500

    add cpu header

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -1,7 +1,14 @@
+#include <stdlib.h>
 #ifndef
 #define _CPU_H
 
 typedef struct {
+  uint16_t PC; // program counter - 16bit address
+  uint8_t SP;  // stack pointer - 8bit
+  uint8_t A;   // Accumilator - 8bit
+  uint8_t X;   // X Register
+  uint8_t Y;   // Y Register
+  uint8_t P;   // Status register
 } cpu_t;
 
 #endif
