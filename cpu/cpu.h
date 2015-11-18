898055122b7194120aeea774097fc7d090512c9f
Udayanga Wickramasinghe <udayanga@Udayangas-MacBook-Pro.local>
2015-11-18 05:19:21 -0500

    including basic interfaces for 6502 instruction set

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -1,6 +1,7 @@
+#ifndef _CPU_H_
+#define _CPU_H_
 #include <stdlib.h>
-#ifndef
-#define _CPU_H
+#include "runtime.h"
 
 typedef struct {
   uint16_t PC; // program counter - 16bit address
@@ -11,4 +12,32 @@ typedef struct {
   uint8_t P;   // Status register
 } cpu_t;
 
+typedef enum {
+  ZeroPage = 0,
+  ZeroPageX,
+  ZeroPageY,
+  AbsoluteX,
+  AbsoluteY,
+  Indirect,
+  Implied,
+  Accum,
+  Relative,
+  XIndirect,
+  YIndirect,
+  IndirectX,
+  IndirectY
+} addr_mode_t;
+
+typedef int (*i_executor)(void *runtime, void *op1, void *op2, void *op3);
+
+/*
+ * A type describing each intruction
+ * */
+typedef struct {
+  int size;         // instruction size in bytes = opcode + operand/s
+  addr_mode_t mode; // addressing mode
+  int cycles;       // execution cpu cycles
+  int cycles_b;     // additional cpu cycles if page_boundry is crossed
+  i_executor exec;  // function implementing the execution of instruction
+} instr_t;
 #endif
---
+++ cpu/runtime.h
@@ -0,0 +1,7 @@
+#ifndef _RUNTIME_H_
+#define _RUNTIME_H_
+
+#include "cpu.h"
+typedef struct { cpu_t *cpu; } runtime_t;
+
+#endif
